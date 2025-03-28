c352b0e4defb0627f8d5a6b947fa54a6bdfe8d8b
coreconsultingit <info@coreconsultingit.com>
2025-03-28 22:04:57 +0000

    fixed the header and image

--- dotnet-interview-react/src/components/Header.tsx
+++ dotnet-interview-react/src/components/Header.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <header style={{ backgroundColor: '#f0f0f0', display: 'flex', alignItems: 'center', padding: '10px 20px' }}>
-      <img src="/images/logo.jpeg" alt="Site Logo" style={{ width: '50px', height: '50px', marginRight: '10px' }} />
-      <h1 style={{ margin: 0, fontSize: '1.5em' }}>.NET Interview Questions</h1>
+    <header style={{ backgroundColor: '#bfbfbf', display: 'flex', alignItems: 'center', padding: '10px 20px' }}>
+      <img src="images/logo.jpeg" alt="Site Logo" style={{ width: '50px', height: '50px', marginRight: '10px' }} />
+      <h1 style={{ margin: 0, fontSize: '1.5em',  color: '#000',  }}>.NET Interview Questions</h1>
     </header>
   );
 };
