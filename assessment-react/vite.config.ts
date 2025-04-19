33fe9af5aa267e9a5e904d1941048b6efc7af1e7
coreconsultingit <info@coreconsultingit.com>
2025-04-20 00:12:32 +0100

    updated build

--- assessment-react/vite.config.ts
+++ assessment-react/vite.config.ts
@@ -5,6 +5,15 @@ import path from 'path';
 export default defineConfig({
   base: '/AssessmentNinja/', // Replace with your repository name
   plugins: [react()],  
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`,
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
