7cb05c74c29b7cfe1075e33aef68ab1d2e34a269
coreconsultingit <info@coreconsultingit.com>
2025-04-20 18:43:00 +0100

    Added the env

--- assessment-react/src/services/ApiService.tsx
+++ assessment-react/src/services/ApiService.tsx
@@ -4,8 +4,7 @@ import axios from 'axios';
 
 class ApiService {
   // Set base URL for all axios calls
-  static baseURL = 'https://localhost:7018/'; // Update this with your API base URL
-//https://smspapi-cxhzera4excgckfw.canadacentral-01.azurewebsites.net/
+  static baseURL = import.meta.env.VITE_API_BASE_URL;
   // Axios instance for custom configuration (like adding headers, timeouts, etc.)
   static axiosInstance = axios.create({
     baseURL: ApiService.baseURL,
